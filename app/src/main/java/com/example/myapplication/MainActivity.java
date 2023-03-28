4e9283b969a114ea085a2c7e1859ae534443307b
Sarahkhj <xladysarah@gmail.com>
2023-03-28 14:07:39 +0300

    new

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+      Button b;
+      EditText e;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.sendbutton);
+        e = findViewById(R.id.personname);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = e.getText().toString();
+                Intent intent = new Intent(getApplicationContext(),SecActivity.class);
+                intent.putExtra("personname",s);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
